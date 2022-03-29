f59efa848a6864f65886e54909b18c74a18f8252
amarcel <amarcel.pro@gmail.com>
2022-03-29 15:59:04 +0200

    Section 2 - Lesson 4

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -1,3 +1,6 @@
+import 'package:decimal/decimal.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_web3/flutter_web3.dart';
 import 'package:frontend/utils.dart';
@@ -19,7 +22,66 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     setState(() => isConnected = widget.isConnected);
+
+    lotteryContract.on("NewPlayer", (index, from, timestamp, event) {
+      setState(() {
+        int currentIndex = int.parse(index.toString());
+
+        lotteries[currentIndex].players = [
+          ...lotteries[currentIndex].players,
+          from.toString()
+        ];
+
+        lotteries[currentIndex].totalTokens +=
+            lotteries[currentIndex].entryPrice;
+      });
+    });
+
+    lotteryContract.on("NewLot", (lot, event) async {
+      String tokenURI = await nftContract.call("tokenURI", [lot[0].toString()]);
+      String readURI = await http.read(Uri.parse(tokenURI));
+
+      setState(() => lotteries
+          .add(LotteryStruct.fromCall(lot, jsonDecode(readURI)["image"])));
+    });
+
+    lotteryContract.on("EndedLot", (lot, event) async {
+      LotteryStruct toRefresh = lotteries.singleWhere(
+          (element) => element.tokenId == int.parse(lot[0].toString()));
+      setState(() {
+        toRefresh.ended = true;
+        toRefresh.winner = lot[4];
+      });
+    });
+
     super.initState();
+  }
+
+  void buyTicket() async {
+    setState(() => mining = true);
+    try {
+      Decimal _entryPrice =
+          (Decimal.parse(lotteries[currentIndex].entryPrice.toString()) *
+              Decimal.ten.pow(18));
+
+      BigInt _allowance = await tokenContract.allowance(
+          await provider!.getSigner().getAddress(), lotteryAddress);
+
+      if (_allowance < _entryPrice.toBigInt()) {
+        TransactionResponse _approve = await tokenContract.approve(
+            lotteryAddress,
+            (Decimal.parse(lotteries[currentIndex].entryPrice.toString()) *
+                    Decimal.ten.pow(18))
+                .toBigInt());
+        await _approve.wait();
+      }
+      TransactionResponse _newPlayer =
+          await lotteryContract.send("newPlayer", [currentIndex]);
+      await _newPlayer.wait();
+      setState(() => mining = false);
+    } catch (e) {
+      setState(() => mining = false);
+    }
   }
 
   void _connectMetamask() async {
@@ -142,7 +204,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       : Padding(
                                           padding: const EdgeInsets.all(8.0),
                                           child: ElevatedButton.icon(
-                                            onPressed: () => {},
+                                            onPressed: () => buyTicket(),
                                             icon: const Icon(
                                                 Icons.attach_money_sharp),
                                             label: const Text("Buy my ticket"),
